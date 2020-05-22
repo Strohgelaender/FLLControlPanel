dabfb4e128bf294e614a2e956786dad95b762e94
rigob <ge35sot@mytum.de>
2020-05-22 16:01:32 +0200

    Ordnendtlichen Code, wer braucht das schon?

--- src/main/java/gui/ControlApplication.java
+++ src/main/java/gui/ControlApplication.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.time.LocalTime;
 
+import javafx.scene.control.CheckBox;
 import org.controlsfx.control.StatusBar;
 
 import javafx.animation.Animation;
@@ -21,68 +22,72 @@ import struct.TimeSlot;
 
 public class ControlApplication extends Application {
 
-	@Override
-	public void start(final Stage stage) throws Exception {
+    @Override
+    public void start(final Stage stage) throws Exception {
 
-		AnchorPane windomRoot = new AnchorPane();
+        AnchorPane windomRoot = new AnchorPane();
 
-		//rot Grid Pane: 0 Top Buttons, 1 Table
+        //rot Grid Pane: 0 Top Buttons, 1 Table
 
-		GridPane root = new GridPane();
+        GridPane root = new GridPane();
 
-		//Buttons
+        //Buttons
+        Button left_arrow = new Button("links");
+        Button right_arrow = new Button("rechts");
+        Button Download_file = new Button("Download Scoreboard");
 
+        CheckBox Autodelay = new CheckBox("Auto");
 
-		//Table
+        //Table
 
-		TableView<TimeSlot> tableView = new TableView<>();
+        TableView<TimeSlot> tableView = new TableView<>();
 
-		TableColumn time = new TableColumn("Time");
-		TableColumn teamA = new TableColumn("Team");
-		TableColumn tableA = new TableColumn("TischA");
-		TableColumn tableB = new TableColumn("TischB");
-		TableColumn teamB = new TableColumn("Team");
+        TableColumn time = new TableColumn("Time");
+        TableColumn teamA = new TableColumn("Team");
+        TableColumn tableA = new TableColumn("TischA");
+        TableColumn tableB = new TableColumn("TischB");
+        TableColumn teamB = new TableColumn("Team");
 
-		teamA.setPrefWidth(200);
+        teamA.setPrefWidth(200);
 
-		tableView.getColumns().addAll(time, teamA, tableA, tableB, teamB);
+        tableView.getColumns().addAll(time, teamA, tableA, tableB, teamB);
 
-		tableView.setPrefWidth(1150);
+        tableView.setPrefWidth(1150);
 
-		tableView.setEditable(true);
+        tableView.setEditable(true);
 
-		root.add(tableView, 0, 2);
+        root.add(tableView, 0, 2);
 
-		root.setPadding(new Insets(10));
+        root.setPadding(new Insets(10));
 
-		StatusBar statusBar = new StatusBar();
-		statusBar.setText("13:42:44");
+        StatusBar statusBar = new StatusBar();
+        statusBar.setText("13:42:44");
 
-		Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, actionEvent -> {
-			LocalTime localTime = LocalTime.now();
-			statusBar.setText(String.format("%s:%s:%s", addZeorsToNumber(localTime.getHour()), addZeorsToNumber(localTime.getMinute()), addZeorsToNumber(localTime.getSecond())));
-		}), new KeyFrame(Duration.seconds(1)));
-		clock.setCycleCount(Animation.INDEFINITE);
-		clock.play();
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, actionEvent -> {
+            LocalTime localTime = LocalTime.now();
+            statusBar.setText(String.format("%s:%s:%s", addZeorsToNumber(localTime.getHour()), addZeorsToNumber(localTime.getMinute()), addZeorsToNumber(localTime.getSecond())));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
 
-		windomRoot.getChildren().addAll(root, statusBar);
-		AnchorPane.setBottomAnchor(statusBar, 0.0);
-		AnchorPane.setLeftAnchor(statusBar, 0.0);
-		AnchorPane.setRightAnchor(statusBar, 0.0);
+        windomRoot.getChildren().addAll(root, statusBar);
+        AnchorPane.setBottomAnchor(statusBar, 0.0);
+        AnchorPane.setLeftAnchor(statusBar, 0.0);
+        AnchorPane.setRightAnchor(statusBar, 0.0);
 
-		Scene scene = new Scene(windomRoot);
+        Scene scene = new Scene(windomRoot);
 
-		stage.setScene(scene);
+        stage.setScene(scene);
 
-		stage.setTitle("FLL Control Panel");
-		stage.setWidth(1150);
-		stage.setHeight(650);
-		stage.show();
-	}
+        stage.setTitle("FLL Control Panel");
+        stage.setWidth(1150);
+        stage.setHeight(650);
+        stage.show();
+    }
 
-	private String addZeorsToNumber(int num) {
-		if (num < 10)
-			return "0" + num;
-		return "" + num;
-	}
+    private String addZeorsToNumber(int num) {
+        if (num < 10)
+            return "0" + num;
+        return "" + num;
+    }
 }
