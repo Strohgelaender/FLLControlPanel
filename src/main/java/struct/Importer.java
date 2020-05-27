73d77d6d1da15a8c93b3eae1b1e9c4953049f20d
Lucas Welscher <lucas.welscher@nuclos.de>
2020-05-28 00:05:34 +0200

    Importer Excel basics

--- src/main/java/struct/Importer.java
+++ src/main/java/struct/Importer.java
@@ -1,22 +1,120 @@
 package struct;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.binary.XSSFBSharedStringsTable;
+import org.apache.poi.xssf.binary.XSSFBSheetHandler;
+import org.apache.poi.xssf.binary.XSSFBStylesTable;
+import org.apache.poi.xssf.eventusermodel.XSSFBReader;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.usermodel.XSSFComment;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 
 public class Importer {
 
 	public static void importFile(File file) {
-		try (FileInputStream fis = new FileInputStream(file)) {
-			XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		try (OPCPackage opcPackage = OPCPackage.open(file)) {
 
-			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+			String xml = null;
+			{
 
+				XSSFBReader reader = new XSSFBReader(opcPackage);
+
+				XSSFBSharedStringsTable sst = new XSSFBSharedStringsTable(opcPackage);
+				XSSFBStylesTable stylesTable = reader.getXSSFBStylesTable();
+				XSSFBReader.SheetIterator iterator = (XSSFBReader.SheetIterator) reader.getSheetsData();
+
+
+				while (iterator.hasNext()) {
+					InputStream is = iterator.next();
+
+					if (!iterator.getSheetPart().getPartName().getName().endsWith("sheet25.bin"))
+						continue;
+
+					String name = iterator.getSheetName();
+					System.out.println(name);
+
+					TestSheetHandler testSheetHandler = new TestSheetHandler();
+					testSheetHandler.startSheet(name);
+
+					XSSFBSheetHandler sheetHandler = new XSSFBSheetHandler(is, stylesTable, iterator.getXSSFBSheetComments(), sst, testSheetHandler, new DataFormatter(), false);
+					sheetHandler.parse();
+					testSheetHandler.endSheet();
+
+					xml = testSheetHandler.toString();
+					break;
+				}
 			}
-		} catch (IOException e) {
+
+			if (xml == null)
+				//TODO show Exeption to User
+				return;
+
+			System.out.println(xml);
+
+			Document document = DocumentHelper.parseText(xml);
+
+			System.out.println(document.getRootElement().getName());
+
+
+		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private static class TestSheetHandler implements XSSFSheetXMLHandler.SheetContentsHandler {
+		private final StringBuilder sb = new StringBuilder();
+
+		public void startSheet(String sheetName) {
+			sb.append("<sheet name=\"").append(sheetName).append("\">");
+		}
+
+		public void endSheet() {
+			sb.append("</sheet>");
+		}
+
+		@Override
+		public void startRow(int rowNum) {
+			sb.append("\n<tr num=\"").append(rowNum).append("\">");
+		}
+
+		@Override
+		public void endRow(int rowNum) {
+			//sb.append("\n</tr num=\"").append(rowNum).append("\">");
+			sb.append("\n</tr>");
+		}
+
+		@Override
+		public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+			formattedValue = (formattedValue == null) ? "" : formattedValue;
+			if (comment == null) {
+				sb.append("\n\t<td ref=\"").append(cellReference).append("\">").append(formattedValue).append("</td>");
+			} else {
+				sb.append("\n\t<td ref=\"").append(cellReference).append("\">")
+						.append(formattedValue)
+						.append("<span type=\"comment\" author=\"")
+						.append(comment.getAuthor()).append("\">")
+						.append(comment.getString().toString().trim()).append("</span>")
+						.append("</td>");
+			}
+		}
+
+		@Override
+		public void headerFooter(String text, boolean isHeader, String tagName) {
+			/*if (isHeader) {
+				sb.append("<header tagName=\"").append(tagName).append("\">").append(text).append("</header>");
+			} else {
+				sb.append("<footer tagName=\"").append(tagName).append("\">").append(text).append("</footer>");
+			}*/
+		}
+
+		@Override
+		public String toString() {
+			return sb.toString();
 		}
 	}
 }
